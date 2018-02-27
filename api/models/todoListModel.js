af0a24f728f29a343005c0498963069da704228a
Aditya Kumar Sharma <adityas@EVOLVUS.COM>
2018-02-27 14:39:51 +0530

    add project page

---
+++ api/models/todoListModel.js
@@ -0,0 +1,32 @@
+'use strict';
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+
+var TaskSchema = new Schema({
+  name: {
+    type: String,
+    required: 'Kindly enter the name of the task'
+  },
+  Created_date: {
+    type: Date,
+    default: Date.now
+  },
+  acc_no: {
+    type: Number,
+    required: 'Kindly enter the account number'
+  },
+  acc_bal: {
+    type: Number,
+    required: 'Kindly enter the account balance'
+  },
+  status: {
+    type: [{
+      type: String,
+      enum: ['pending', 'ongoing', 'completed']
+    }],
+    default: ['pending']
+  }
+});
+
+module.exports = mongoose.model('Tasks', TaskSchema);
